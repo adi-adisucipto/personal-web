cda83a6fa96d15c474fbabdd1d30388850ce7888
adi-adisucipto <suciptoadi550@gmail.com>
2025-10-20 12:06:55 +0700

    perubahan kecil

--- src/sections/AboutMe.tsx
+++ src/sections/AboutMe.tsx
@@ -3,9 +3,9 @@ import body from '../../public/images/body-adi.webp'
 
 function AboutMe() {
   return (
-    <section id="about" className="bg-gray-50 md:py-20 py-16">
+    <section id="about" className="bg-gray-50 dark:bg-gray-800 md:py-20 py-16">
         <h2 className='px-4 md:px-8 w-full text-center mb-10 md:text-xl text-lg'>About Me</h2>
-        <div className="w-full dark:bg-gray-800">
+        <div className="w-full ">
             <div className="mx-auto px-4 md:px-8 flex gap-12 w-full max-w-7xl flex-col md:flex-row justify-between items-center">
                 <div className="relative h-[380px] w-[320px] md:h-[460px] md:w-[380px]">
                     <Image src={body} alt="body" style={{objectFit: 'cover'}} className="absolute z-10 h-[360px] w-[280px] border-8 border-gray-50 dark:border-gray-800 max-md:left-5 md:right-0 md:h-[420px] md:w-[340px]"></Image>
--- src/sections/Contact.tsx
+++ src/sections/Contact.tsx
@@ -5,7 +5,7 @@ function Contact() {
     <section id='contact' className='max-w-3xl mx-auto py-16 md:py-20'>
       <h2 className='px-4 md:px-8 w-full text-center mb-10 md:text-xl text-lg'>Contact</h2>
         <div className='flex flex-col justify-center items-center gap-10 p-4'>
-            <p className='text-center text-gray-800/70 text-xl'>Apa selanjutnya? Jangan ragu untuk menghubungi saya jika Anda mencari pengembang, memiliki pertanyaan, atau sekadar ingin terhubung.</p>
+            <p className='text-center text-gray-800/70 text-xl dark:text-white'>Apa selanjutnya? Jangan ragu untuk menghubungi saya jika Anda mencari pengembang, memiliki pertanyaan, atau sekadar ingin terhubung.</p>
             <div className='flex text-3xl justify-between items-center gap-5'>
                 <Mail/>
                 <span>suciptoadi550@gmail.com</span>
